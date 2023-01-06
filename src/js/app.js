fa4d87cf9765121019560cff468e8007227d4821
Nikolay Klymenko <nikklimenko89@gmail.com>
2023-01-06 23:14:35 +0200

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+import {Game} from "./modules/game.js";
+
+class App{
+
+    settings = {
+        positionsCount: 33,
+        positionsSize: 27,
+
+    }
+
+    constructor() {
+        const canvas = document.createElement('canvas');
+        canvas.setAttribute('width', (this.settings.positionsCount * this.settings.positionsSize).toString());
+        canvas.setAttribute('height',(this.settings.positionsCount * this.settings.positionsSize).toString());
+        document.getElementById('container').appendChild(canvas);
+
+        const context = canvas.getContext('2d');
+
+        new Game(context, this.settings);
+
+    }
+
+}
+
+(new App());
